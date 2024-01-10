d70b3a312b75175733510c7d05f062c1dba1bd5b
sojer <seb.maz1996@gmail.com>
2024-01-10 22:47:58 +0100

    [HIK-1] Create backend project

---
+++ src/main/java/com/hikeup/backend/HikeupBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hikeup.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikeupBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HikeupBackendApplication.class, args);
+    }
+
+}

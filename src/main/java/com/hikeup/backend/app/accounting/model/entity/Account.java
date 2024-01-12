f37d7048a75eaba21460e9be4fa0c57d4f9def2f
sojer <seb.maz1996@gmail.com>
2024-01-12 20:08:57 +0100

    [HIK-2] Refactor existing account-related classes

--- src/main/java/com/hikeup/backend/app/accounting/model/entity/Account.java
+++ src/main/java/com/hikeup/backend/app/accounting/model/entity/Account.java
@@ -3,9 +3,12 @@ package com.hikeup.backend.app.accounting.model.entity;
 import com.hikeup.backend.core.common.model.entity.BaseEntity;
 import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Project: hikeup-backend
@@ -15,30 +18,30 @@ import java.util.Collection;
  **/
 @Entity
 @Table(name = "accounts")
-public class Account extends BaseEntity<Account> implements UserDetails {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+public class Account extends BaseEntity implements UserDetails {
 
     private String username;
     private String password;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "account_id")
+    private List<Role> roles;
 
     public Account() {
         // empty constructor
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
     @Override
     public String getUsername() {
         return username;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
     }
 
     public void setUsername(String username) {
@@ -76,6 +79,8 @@ public class Account extends BaseEntity<Account> implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getAuthority().name()))
+                .collect(Collectors.toList());
     }
 }
